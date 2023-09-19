94e9e6ed2ebc1e2057c1114f0b1a6156c4b6b2a2
kanhaiyasukhwal <sukhwalkanhaiya011@gmail.com>
2023-09-19 16:59:50 +0530

    first commit

--- A.java
+++ A.java
@@ -3,5 +3,11 @@ class A
 	public static void main(String[] args) 
 	{
 		System.out.println("i am into the lara");
+		System.out.println("i am into the lara");
+		System.out.println("i am into the lara");
+		System.out.println("i am into the lara");
+		System.out.println("i am into the lara");
+		System.out.println("i am into the lara");
+		System.out.println("i am into the lara");
 	}
 }
